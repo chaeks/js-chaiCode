c4268971a82674dfd955e4bc2d7ead62acaf466e
chaeks <44608182+chaeks@users.noreply.github.com>
2024-08-17 17:21:59 +0000

    Variable and Datatype

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 23432
+let accountEmail = 'ckkushw@wge.gqe'
+var accountPassword = "322323"
+accountCity = 'Jaipur'
+
+
+let accountState;
+// accountId = 231
+
+accountEmail = 'eqgqe@qge.eq'
+accountPassword = "412124"
+accountCity = 'Delhi'
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); //we are using NodeJs not browser
+
+console.log(3 + 3)
+console.log('Hitesh')
+
+let name = 'Hitesh'
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => Standalone value
+// undefined => value not assined yet
+// Symbol => unique
+// Object
+
+console.log(typeof "Hitesh") //String
+console.log(typeof age) //number
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
+
+
+
+
+
+
+
